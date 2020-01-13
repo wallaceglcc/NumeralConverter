0c938e923e379c16a44f7643ab798959416738d2
Gordon Wallace <gordon.wallace@feynmangroup.com>
2020-01-13 12:41:19 -0800

    Beta code no UI

---
+++ RomanNumeralCOnverter/RomanNumeralCOnverter/NumberConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace RomanNumeralConverter
+{
+    class NumberConverter
+    {
+        static List<string> numerals = new List<string> {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
+        
+        public static string ConvertNumberToNumeral(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return ""; //Field is empty/whitespace
+            }
+
+            if (int.TryParse(text, out int num)) //Convert text field to num
+            {
+                if (num > 0 && num < 11) //Check if num 1-10
+                {
+                    return numerals[num - 1]; //Return converted numeral
+                }
+                else return "OR"; //Outside range
+            }
+            else return "NN"; //Not a number
+        }
+    }
+}
---
+++ RomanNumeralCOnverter/RomanNumeralCOnverter/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace RomanNumeralConverter
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+            string convertedNumeral = ""; //To hold returned numeral
+
+            btnConvertNumber.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                convertedNumeral = NumberConverter.ConvertNumberToNumeral(txtDigit); //Convert whatever is in the text field
+
+                txtDigit.ResignFirstResponder(); //Get rid of the keyboard
+
+                if (convertedNumeral != "") //Check if something was entered
+                {
+                    if (convertedNumeral != "NN") //Check if it was a number
+                    {
+                        if (convertedNumeral != "OR") //Check if it was 1-10
+                        {
+                            txtDigit.text = convertedNumeral; //Set text field to converted numeral
+                        }
+                        else txtDigit.text = "Number must be 1-10";
+                    }
+                    else txtDigit.text = "Entry must be a number 1-10";
+                }
+                else txtDigit.text = "Enter a number 1-10";
+            }
+
+            //Quick clear the text field
+            btnClear.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                txtDigit.text = "";
+            }
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
